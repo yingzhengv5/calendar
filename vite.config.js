e841b4814bf54e8ec8e5c3f34bcd8a821c266af8
Ying Zheng <zhengyingwhv@gmail.com>
2025-06-05 09:52:52 +1200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+
+  base: "./",
   test: {
     globals: true,
     environment: "jsdom",
